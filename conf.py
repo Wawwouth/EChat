ab50ecd0d18eba9828378d3efeaf3b6ea3faef48
Wawwouth <knyxe.hequinoxteam@gmail.com>
2015-02-28 00:07:07 +0100

    New IP

--- conf.py
+++ conf.py
@@ -6,9 +6,14 @@ in_cmd_hooks = {}
 out_cmd_hooks = {}
 
 # host = "chatv2.eclypsia.com"
+<<<<<<< HEAD
+#ec_host = "95.142.101.119"
+ec_host = "node.eclypsia.com"
+=======
 # ec_host = "95.142.101.119"
 # ec_host = "54.76.195.245"
 ec_host = "54.77.18.242"
+>>>>>>> 24ea7eb6d379b342c26a6b4b7448cf670e15890e
 ec_port1 = 12564
 ec_port2 = 12565
 ec_port_affiliate = 12566
@@ -23,19 +28,24 @@ msg_event = "send_chat_message"
 # No spaces, no special characters
 # { "roomalias" : "roomID"}
 aliases = { "ectv" : "23",
+			"taipouz" : "17",
+			"ruurk" : "189",
+			"skyyart" : "5",
+			"mr-freeze" : "5220",
+			"clchiva" : "1346",
 			"ectv2" : "2675",
 			"ircarmy" : "1337",
 			"lrbtv" : "5365",
-			"nanashor" : "3",
-			"nanashortv" : "3",
 			"velahan" : "1059",
+			"drfeelgood" : "5212",
 			"jiraya" : "6",
-			"mizuty" : "3080",
-			"gbt" : "6495",
 			"leagueofladies" : "5654",
-			"nailow" : "3749",
-			"le-tol" : "5356",
-			"zerator" : "1"
+			"zerator" : "1",
+			"chelxie" : "3037",
+			"maxildan" : "16",
+			"aeterna" : "99",
+			"kwev" : "3300",
+			"ectv3" : "9442"
 			}
 
 def add_out_hook(cmd, hook):
